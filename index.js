e25dd82c58a4e35c04a90d4949ca4e3cd239c5db
Iramal <9588504@gmail.com>
2019-11-16 21:43:07 +0500

    update index.js

--- index.js
+++ index.js
@@ -1,29 +1,16 @@
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const { u: User } = require('./models/User');
 const [{ Server: h1 }, x] = [require('http'), require('express')];
-
 const request = require('request');
 const url = require('url');
+const bodyParser = require('body-parser');
+const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('passport-local');
-
-const Router = x.Router();
-const PORT = 4321;
-const { log } = console;
-const hu = { 'Content-Type': 'text/html; charset=utf-8' };
-const app = x();
-
-const checkAuth = (req, res, next) => {
-  if (req.session.auth == 'ok') {
-    next();
-  } else {
-    res.redirect('/login');
-  }
-};
+const { u: User } = require('./models/User');
+const bcrypt = require('bcrypt');
+const salt = bcrypt.genSaltSync(10);
 
 const ensureAuth = (r, res, done) => {
-  if (!r.isAuthenticated()) return res.redirect('/login');
+  if (!r.isAuthenticated()) return res.redirect('/locallogin');
   return done();
 };
 
@@ -33,31 +20,30 @@ passport.use(new passportLocal.Strategy({
 },
 async (login, password, done) => {
   let user;
+  const cryptedPassword = bcrypt.hashSync(password, salt);
   try {
     user = await User.findOne({ login });
-    console.log(user, password) 
-  } catch (e) { 
-    console.log(e) 
+  } catch (e) {
     return done('!! ' + e);
-    
   }
-  console.log(user, password) 
-  if (!user || user.password !== password)  {
-    console.log(user, password) 
+  if (!user || bcrypt.hashSync(user.password, salt) !== cryptedPassword) {
     return done(null, false); // оба случая не найден юзер и неверен пароль
-  } console.log(user, password) 
+  }
   return done(null, user);
-
 }
 ));
-console.log(user, password)
 
 passport.serializeUser((user, done) => done(null, user._id));
 passport.deserializeUser((_id, done) => User.findById(_id, (err, user) => done(err, user)));
 
+const Router = x.Router();
+const PORT = 4001;
+const { log } = console;
+const hu = { 'Content-Type': 'text/html; charset=utf-8' };
+const app = x();
 Router
   .route('/')
-  .get(r => r.res.end('Привет мир!'));
+  .get(r => r.res.end(`<a href="/locallogin">Авторизоваться по этой ссылке</a><p><a href="/users">Users из базы данных</a></p>`));
 Router
   .route('/err')
   .get(r => r.res.end('Увы, не получилось! Ещё раз: <a href="/profile">Профиль</a>'));
@@ -65,15 +51,18 @@ app
   .use((r, rs, n) => rs.status(200).set(hu) && n())
   .use(x.static('.'))
   .use(bodyParser.json())
-  .use(bodyParser.urlencoded({ extended: true}))
-  .use(session({ secret: 'mysecret', resave: true, saveUninitialized: true}))
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(session({ secret: 'mysecret', resave: true, saveUninitialized: true }))
   .use(passport.initialize())
   .use(passport.session())
 
   .use('/', Router)
 
-  .get('/login', (req, res) => res.render('login'))
-  .post('/login/check/', passport.authenticate('local', { successRedirect: '/profile', failureRedirect: '/err' }))
+  .get('/locallogin', (req, res) => res.render('login'))
+  .post('/locallogin/check', passport.authenticate('local', { successRedirect: '/profile', failureRedirect: '/err' }))
+
+  .get('/profile', ensureAuth, r => r.res.send(`<a href="/logout">Хотите выйти, ${r.user.login}?</a>`))
+
   .get('/logout', (r, res) => {
     r.logout();
     res.redirect('/');
@@ -85,7 +74,7 @@ app
     });
     res.render('users', {users: answer});
   })
-  .get('/profile', ensureAuth, r => r.res.send(`<a href="/logout">Хотите выйти, ${r.user.login}?</a>`))
+
   .use(({ res: r }) => r.status(404).end('Пока нет!'))
   .use((e, r, rs, n) => rs.status(500).end(`Ошибка: ${e}`))
   .set('view engine', 'pug')
